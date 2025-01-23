05cea9d7e013914395b714fd3c83cdfafecdff02
Kamuri Amorim <myk.gata14@gmail.com>
2025-01-23 18:48:28 -0300

    feat: add tf function

---
+++ functions/tf.fish
@@ -0,0 +1,3 @@
+function tf --description "opentofu wrapper with notification support"
+    tofu $argv && notify-send -a Terraform Terraform OK && return 0 || notify-send -a Terraform Fail && return 1
+end
