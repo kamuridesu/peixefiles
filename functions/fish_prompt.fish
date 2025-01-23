272e214a75a0f2f35075b51a96abf484fb89d27e
Kamuri Amorim <myk.gata14@gmail.com>
2025-01-23 18:24:09 -0300

    feat: add some git abrr

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,20 +1,46 @@
-function fish_prompt --description 'Informative prompt'
-    #Save the return status of the previous command
-    set -l last_pipestatus $pipestatus
-    set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
+function fish_prompt
+    set -l last_status $status
 
-    if functions -q fish_is_root_user; and fish_is_root_user
-        printf '%s@%s %s%s%s# ' $USER (prompt_hostname) (set -q fish_color_cwd_root
-                                                         and set_color $fish_color_cwd_root
-                                                         or set_color $fish_color_cwd) \
-            (prompt_pwd) (set_color normal)
-    else
-        set -l status_color (set_color $fish_color_status)
-        set -l statusb_color (set_color --bold $fish_color_status)
-        set -l pipestatus_string (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
+    set -l normal (set_color normal)
+    set -l usercolor (set_color $fish_color_user)
 
-        printf '[%s] %s%s@%s %s%s %s%s%s \n> ' (date "+%H:%M:%S") (set_color brblue) \
-            $USER (prompt_hostname) (set_color $fish_color_cwd) $PWD $pipestatus_string \
-            (set_color normal)
+    set -l delim \U25BA
+    # If we don't have unicode use a simpler delimiter
+    string match -qi "*.utf-8" -- $LANG $LC_CTYPE $LC_ALL; or set delim ">"
+
+    fish_is_root_user; and set delim "#"
+
+    set -l cwd (set_color $fish_color_cwd)
+    if command -sq sha256sum
+        # randomized cwd color
+        set -l shas (pwd -P | sha256sum | string sub -l 6 | string match -ra ..)
+        # Increase color a bit so we don't get super dark colors.
+        # Really we want some contrast to the background (assuming black).
+        set -l col (for f in $shas; math --base=hex "min(255, 0x$f + 0x30)"; end | string replace 0x '' | string pad -c 0 -w 2 | string join "")
+
+        set cwd (set_color $col)
     end
+
+    # Prompt status only if it's not 0
+    set -l prompt_status
+    test $last_status -ne 0; and set prompt_status (set_color $fish_color_error)"[$last_status]$normal"
+
+    # Only show host if in SSH or container
+    # Store this in a global variable because it's slow and unchanging
+    if not set -q prompt_host
+        set -g prompt_host ""
+        if set -q SSH_TTY
+            or begin
+                command -sq systemd-detect-virt
+                and systemd-detect-virt -q
+            end
+            set -l host (hostname)
+            set prompt_host $usercolor$USER$normal@(set_color $fish_color_host)$host$normal":"
+        end
+    end
+
+    # Shorten pwd if prompt is too long
+    set -l pwd (prompt_pwd)
+
+    echo -n -s $prompt_host $cwd $pwd $normal $prompt_status $delim
 end
