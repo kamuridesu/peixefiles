a7891d46df064c3a90fa8b6a8825e1344de01155
Kamuri Amorim <myk.gata14@gmail.com>
2025-01-23 16:27:32 -0300

    feat: init path variables

---
+++ conf.d/path.env.fish
@@ -0,0 +1,11 @@
+fish_add_path /opt/flutter/bin $HOME/.bin 
+fish_add_path /home/$HOME/.local/lib/python3.10/site-packages/bin 
+fish_add_path /usr/bin/chromium 
+fish_add_path $HOME/.bin/bin/studio.sh 
+fish_add_path $HOME/.jbang/bin 
+fish_add_path /home/$HOME/bin 
+fish_add_path /usr/local/go/bin 
+fish_add_path (go env GOPATH)/bin 
+fish_add_path $HOME/.local/bin 
+fish_add_path $HOME/.cargo/bin 
+fish_add_path /opt/nvim-linux64/bin 
