4259bf1173d2bc7d6c68380c82533c5ad9ffc106
Kamuri Amorim <myk.gata14@gmail.com>
2025-01-24 09:22:55 -0300

    feat: clean

--- functions/tf.fish
+++ functions/tf.fish
@@ -2,9 +2,3 @@ function tf --description "opentofu wrapper with notification support"
     tofu $argv && notify-send -a Terraform Terraform OK && return 0 || notify-send -a Terraform Fail && return 1
 end
 
-function set_project_env -d "Autoload project env"
-    if test -f "./.sdkmanrc"
-        sdk env
-        return
-    end
-end
