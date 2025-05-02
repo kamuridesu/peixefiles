e4e0d6dcb813e278e5f79d1c0da10a8e03a86226
Luiz Eduardo Amorim Ramos Miranda <luiz.miranda@grupomateus.com>
2025-05-02 08:12:41 -0300

    fix: updated via dotsyncer

--- conf.d/load_env.fish
+++ conf.d/load_env.fish
@@ -1,2 +1,3 @@
-source "$__fish_config_dir/conf.d/$hostname/config.fish"
-
+if test -d "$__fish_config_dir/conf.d/$hostname/config.fish"
+    source "$__fish_config_dir/conf.d/$hostname/config.fish"
+end
