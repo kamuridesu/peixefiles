092cc5595646c1807c49f1f6e84680dde75d945d
Kamuri Amorim <myk.gata14@gmail.com>
2025-07-11 16:51:14 -0300

    feat: add confirmation to shredir

--- functions/shredir.fish
+++ functions/shredir.fish
@@ -10,7 +10,7 @@ function shredir --description "Wraps shred to delete dirs and shred files"
     set -l RM rm -r
     set -l SHRED shred --zero --iterations=10 -u --random-source=/dev/urandom 
     if set -q _flag_force
-        echo "Warning: All commands will force removed"
+        echo "Warning: All files will be force removed"
         set RM "$RM -f"
         set SHRED "$SHRED -f"
     end
@@ -18,6 +18,15 @@ function shredir --description "Wraps shred to delete dirs and shred files"
     set -l FILES (find $DIR -type f)
     if test ! $status -eq 0
         return $status
+    end
+
+    set -l CONFIRM "N"
+
+    echo "$(count $FILES) files will be FOREVER DELETED, are you sure? [y/N]: "
+    set -l CONFIRM (read)
+
+    if test $CONFIRM != "y"
+        return 0
     end
 
     for file in $FILES
