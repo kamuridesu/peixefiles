a08cd59ecf47ede913cd1c4f0779be045e47041c
Kamuri Amorim <myk.gata14@gmail.com>
2025-10-07 20:17:43 -0300

    feat: use ggl abbr

--- functions/ggl.fish
+++
@@ -1,3 +0,0 @@
-function ggl --wraps='git pull origin (git branch --show-current)' --description 'alias ggl=git pull origin (git branch --show-current)'
-  git pull origin (git branch --show-current) $argv; 
-end
