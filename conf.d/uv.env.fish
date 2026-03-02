5e41e184e87c2d375b4eeb9ea1150432b964d622
Kamuri Amorim <myk.gata14@gmail.com>
2026-03-02 20:04:10 -0300

    fix: check for uv path

--- conf.d/uv.env.fish
+++ conf.d/uv.env.fish
@@ -2,6 +2,10 @@ if test -f "$HOME/.cargo/env.fish"
 	source "$HOME/.cargo/env.fish"
 end
 
-source "$HOME/.local/bin/env.fish"
+if test -f "$HOME/.local/bin/env.fish"
+    source "$HOME/.local/bin/env.fish"
+end
 
-fish_add_path "$HOME/.local/bin"
+if test -d "$HOME/.local/bin"
+    fish_add_path "$HOME/.local/bin"
+end
