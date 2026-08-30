59fbd4cff56fa7d741da08b125945c70fb67a4a4
Kamuri Amorim <myk.gata14@gmail.com>
2026-08-30 16:46:59 -0300

    feat: add clock to right prompt and show number of running containers

--- functions/fish_greeting.fish
+++ functions/fish_greeting.fish
@@ -1,6 +1,7 @@
 function fish_greeting -d "What's up, fish?"
     set_color $fish_color_autosuggestion
     uname -nmsr
+    echo "Total running containers: $(math $(docker ps | wc -l) - 1)"
 
     command -q uptime
     and command uptime
---
+++ functions/fish_right_prompt.fish
@@ -0,0 +1,6 @@
+function fish_right_prompt
+    set_color brblack
+    date "+%H:%M:%S"
+    set_color normal
+end
+
